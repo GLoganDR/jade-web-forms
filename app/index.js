4ee4e44d2be13e96731d7da3a7d3c94684ffa1f6
Logan Richardson <g.logan.richardson@gmail.com>
2014-08-01 12:02:34 -0500

    fixed

--- app/index.js
+++ app/index.js
@@ -21,35 +21,43 @@ app.get('/calc', function(req, res){
   res.render('calc');
 });
 
+app.post('/calc', function(req, res){
+  var x = req.body.x * 1;
+  var y = req.body.y * 1;
+  var ans;
+
+  switch(req.body.op){
+    case '+':
+      ans = x + y;
+      break;
+    case '-':
+      ans = x - y;
+      break;
+    case '*':
+      ans = x * y;
+      break;
+    case '/':
+      ans = x / y;
+  }
+
+  res.render('calc', {x:x, y:y, op:req.body.op, ans:ans});
+});
+
 app.get('/boxes', function(req, res){
-  res.render('boxes');
+  res.render('box1');
 });
 
 app.post('/boxes', function(req, res){
-  var count = req.body.count * 1;
   var colors = req.body.colors.split(',');
-  var heights = req.body.height.split('-');
   var widths = req.body.width.split('-');
+  var heights = req.body.height.split('-');
+  var count = req.body.count * 1;
 
   colors = colors.map(function(c){return c.trim();});
   widths = widths.map(function(n){return n * 1;});
   heights = heights.map(function(n){return n * 1;});
 
-  res.render('pbox', {colors:colors, widths:widths, heights:heights, count:count});
-});
-
-app.post('/calc', function(req, res){
-  var ans = 0;
-  if(req.body.symbol === '+'){
-    ans = (req.body.x * 1) + (req.body.y * 1);
-  }else if(req.body.symbol === '-'){
-    ans = (req.body.x * 1) - (req.body.y * 1);
-  }else if(req.body.symbol === '*'){
-    ans = (req.body.x * 1) * (req.body.y * 1);
-  }else{
-    ans = (req.body.x * 1) / (req.body.y * 1);
-  } 
-res.render('calc', {ans:ans});
+  res.render('box2', {colors:colors, widths:widths, heights:heights, count:count});
 });
 
 var port = process.env.PORT;
@@ -57,4 +65,3 @@ var port = process.env.PORT;
 app.listen(port, function(){
   console.log('Express is now listening on PORT', port);
 });
-
